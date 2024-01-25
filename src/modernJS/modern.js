32b4a1e5dc7acb879cfedf37ed07449e3c2d0932
Thomas Edward Bradley <alu0101408248@ull.edu.es>
2024-01-25 20:50:30 +0000

    Added base files and folders

---
+++ src/modernJS/modern.js
@@ -0,0 +1,14 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Thomas Edward Bradley
+ * @author Daniel David Sarmiento Barrera
+ * @since Jan 29 2024
+ * @desc E
+ * @see {@link https://github.com/ULL-ESIT-PAI-2023-2024/2023-2024-pai-introduction-to-js-bradleysarmiento.git}
+ */
+
+'use strict';
